b505d08ae2eab74b2f390add08306798f77b617c
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-20 16:24:48 +0100

    minWeight bike printed

--- js/snack1.js
+++ js/snack1.js
@@ -3,12 +3,16 @@ console.log('OK JS!')
     Snack 1
     Creare un array di oggetti:
         Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-        Stampare ain console la bici con peso minore utilizzando destructuring e template literal
+        Stampare in console la bici con peso minore utilizzando destructuring e template literal
     */
 
 const bikes = [{
         nome: 'merida',
         peso: 20,
+    },
+    {
+        nome: 'decathlon',
+        peso: 10,
     },
     {
         nome: 'trek',
@@ -19,4 +23,21 @@ const bikes = [{
         peso: 15,
     },
 ]
-console.table(bikes);
+
+
+let min = 0;
+let nameMin = '';
+for (let i = 0; i < bikes.length; i++) {
+    const { nome, peso } = bikes[i];
+    const nextPeso = bikes[i + 1].peso;
+    // console.log(min);
+    if (nextPeso > peso) {
+        min = peso;
+        nameMin = nome;
+        console.log(`La bicicletta ${nameMin} è quella che pesa di meno, ovvero: ${min} kg `);
+    }
+}
+
+
+
+// console.log(`La bicicletta con peso minore è: ${nome} e pesa: ${peso} `);
