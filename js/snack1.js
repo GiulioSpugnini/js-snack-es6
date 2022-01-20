3861c9478511517b24ad4aa4c27a0947a9ae0bfe
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-20 14:47:46 +0100

    array of object of bikes created

--- js/snack1.js
+++ js/snack1.js
@@ -1,1 +1,22 @@
 console.log('OK JS!')
+    /*
+    Snack 1
+    Creare un array di oggetti:
+        Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+        Stampare ain console la bici con peso minore utilizzando destructuring e template literal
+    */
+
+const bikes = [{
+        nome: 'merida',
+        peso: 20,
+    },
+    {
+        nome: 'trek',
+        peso: 25,
+    },
+    {
+        nome: 'scott',
+        peso: 15,
+    },
+]
+console.table(bikes);
